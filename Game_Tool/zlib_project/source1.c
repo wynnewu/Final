02acdc6be43e2e7bba788227b31300e03144e478
wynnewu <wynnmlgb@hotmail.com>
2016-12-13 20:21:58 -0800

    123456

--- Game_Tool/zlib_project/source1.c
+++ Game_Tool/zlib_project/source1.c
@@ -6,7 +6,7 @@
 #if defined(MSDOS) || defined(OS2) || defined(WIN32) || defined(__CYGWIN__)
 #  include <fcntl.h>
 #  include <io.h>
-#  define SET_BINARY_MODE(file) setmode(fileno(file), O_BINARY)
+# define SET_BINARY_MODE(file) setmode(fileno(file), O_BINARY)
 #else
 #  define SET_BINARY_MODE(file)
 #endif
